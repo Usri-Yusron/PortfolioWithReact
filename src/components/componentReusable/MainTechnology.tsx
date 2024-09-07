8115ce20886908abf867d4353166f0a8cc51a1a9
Muhamad Usri Yusron <muhamad.usri@mhs.itenas.ac.id>
2024-09-07 15:13:20 +0700

    nambahin padding untuk MainTechnology

--- src/components/componentReusable/MainTechnology.tsx
+++ src/components/componentReusable/MainTechnology.tsx
@@ -5,7 +5,7 @@ import { InteractiveIcons } from "./Interactive-icon";
 
 export function MainTechnology() {
   return (
-    <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-background md:shadow-xl">
+    <div className="relative flex px-4 h-screen w-full flex-col items-center justify-center overflow-hidden bg-background md:shadow-xl">
       <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-7xl font-bold leading-none tracking-tighter text-transparent">
         technologies that I can use
       </span>
