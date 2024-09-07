d628c15ea431f8b054e46cb6c571c6776a3c3da8
Muhamad Usri Yusron <muhamad.usri@mhs.itenas.ac.id>
2024-09-07 15:00:33 +0700

    hapus file react.svg

--- src/components/componentReusable/Footer.tsx
+++ src/components/componentReusable/Footer.tsx
@@ -6,9 +6,8 @@ export function Footer() {
     <div className="h-screen  w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-          Perjalanan Selesai
+          The end...
         </h1>
-        <p></p>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
           Terimakasih untuk kamu yang sudah menjelajahi website saya sampai akhir, saya harap perjalanan kamu dengan saya tidak akan berakhir sampai
           sini saja, mari kita berkenalan lebih jauh untuk menciptakan keajaiban lainnya dengan mengklik tombol yang ada di bawah ini 👇.
